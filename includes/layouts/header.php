0d17bea29d9774776c1a2f9a8947924d35b3dce6
Cyril Gardesey <cyrilgardesey@gmail.com>
2023-05-08 01:43:51 +0000

    First Commit

---
+++ includes/layouts/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Widget Corp</title>
+    <link rel="stylesheet" href="stylesheets/public.css" media="all" type="text/css" />
+</head>
+
+<body>
+    <div id="header">
+         <h1>Widget Corp</h1>
+    </div>
